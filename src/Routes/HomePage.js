3be21918b13cb4544bc6230f4851b2af0ceed2ea
Chris Lynch - Laptop <chrislynchcodes@gmail.com>
2022-04-14 19:13:55 +0100

    homepage update camera test

--- src/Routes/HomePage.js
+++ src/Routes/HomePage.js
@@ -1,21 +1,41 @@
 import React from 'react'
 
 export const HomePage = () => {
+
+  const Camera = async () => {
+    navigator.getUserMedia(
+      { video: true, audio: true },
+      stream => {
+        const localVideo = document.getElementById("local-video");
+        if (localVideo) {
+          localVideo.srcObject = stream;
+        }
+
+        stream.getTracks().forEach(track => this.state.peerConnection.addTrack(track, stream));
+
+      },
+      error => {
+        console.warn(error.message);
+      }
+    );
+  }
   return (
 
 
 
     <div className='grid grid-cols-3 gap-8 mb-8'>
-     
+
       <div></div>
-     
+
       <div>
         <h1 className='text-3xl'>Homepage</h1>
-        <button className="btn">Button</button>
+        <button id="get-access" onClick={() => { Camera() }} className="btn">Camera</button>
+        <video autoPlay id='local-video'/>
+        <video autoplay></video>
         <button className="btn btn-primary">Primary</button>
         <button className="btn btn-secondary">Secondary</button>
         <button className="btn btn-accent">Accent</button>
-      
+
       </div>
 
       <div></div>
